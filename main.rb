eafea062d88fb243789233dd801ad991bcb9fbcd
boztopuz <boztopuz@gmail.com>
2024-10-21 09:50:01 +0300

    Feedback fixed

--- main.rb
+++ main.rb
@@ -24,12 +24,8 @@ def runCommand(command)
     end
 end
 
-options = ""
-tuist_path = env_has_key("AC_TUIST_PATH")
+tuist_path = env_has_key("AC_TUIST_PATH") || abort("Missing AC_TUIST_PATH")
 tuist_commands = env_has_key("AC_TUIST_COMMANDS") || abort("Missing AC_TUIST_COMMANDS")
 
-if tuist_path
-    options += "-p #{tuist_path}"
-end
 
-runCommand("#{tuist_commands} #{options}")
+runCommand("#{tuist_commands} -p #{tuist_path}")
